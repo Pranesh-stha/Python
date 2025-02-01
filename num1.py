d941d838fc4a9d383e7bd3c0bc83fa2bb0eb3b12
Pranesh Shrestha <157561033+Pranesh-stha@users.noreply.github.com>
2025-02-01 13:49:14 +0545

    Delete num1.py

--- num1.py
+++
@@ -1,9 +0,0 @@
-print("Enter a decimal number:")
-Decimal = float(input())
-
-integer = int(Decimal)
-string = str(Decimal)
-
-print("Decimal number: ", Decimal)
-print("Integer number: ", integer)
-print("String number: ", string)
